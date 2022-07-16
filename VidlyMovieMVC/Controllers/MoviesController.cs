4db8b365923c04835e7b0ce4bdf5f4a1ae856e09
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-07-16 20:00:09 +0200

    Action Parameters

--- VidlyMovieMVC/Controllers/MoviesController.cs
+++ VidlyMovieMVC/Controllers/MoviesController.cs
@@ -26,5 +26,25 @@ namespace VidlyMovieMVC.Controllers
         {
             return HttpNotFound();
         }
+
+        public ActionResult Edit(int id)
+        {
+            return Content($"id = {id}");
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
     }
 }
