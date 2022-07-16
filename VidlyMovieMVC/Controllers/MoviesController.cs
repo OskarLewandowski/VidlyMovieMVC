593c4156bf47c3c2daaf7f7f693ec8972ca0b960
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-07-16 17:46:34 +0200

    Add project files.

---
+++ VidlyMovieMVC/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyMovieMVC.Models;
+
+namespace VidlyMovieMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        [HttpGet]
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Id = 1, Name = "Matrix" };
+            return View(movie);
+        }
+    }
+}
