2ac43afefe38013007cb3949ef2741eb16c1c870
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-09-07 15:25:23 +0200

    [Task] form to add or edit movie

--- VidlyMovieMVC/Controllers/MoviesController.cs
+++ VidlyMovieMVC/Controllers/MoviesController.cs
@@ -23,6 +23,56 @@ namespace VidlyMovieMVC.Controllers
             _context.Dispose();
         }
 
+        [HttpPost]
+        public ActionResult Save(Movie movie)
+        {
+            if (movie.Id == 0)
+            {
+                movie.DateAdded = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
+                movieInDb.Name = movie.Name;
+                movieInDb.GenreId = movie.GenreId;
+                movieInDb.NumberInStock = movie.NumberInStock;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+            }
+
+            _context.SaveChanges();
+
+            return RedirectToAction("Movie", "Movies");
+        }
+
+        public ViewResult New()
+        {
+            var genres = _context.Genres.ToList();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Genres = genres
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(c => c.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var viewModel = new MovieFormViewModel
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
+            };
+
+            return View("MovieForm", viewModel);
+        }
+
         // GET: Movies
         [Route("Movies")]
         public ViewResult Movie()
@@ -40,7 +90,6 @@ namespace VidlyMovieMVC.Controllers
                 return HttpNotFound();
 
             return View(movie);
-
         }
 
         //private IEnumerable<Movie> GetMovies()
@@ -76,10 +125,10 @@ namespace VidlyMovieMVC.Controllers
             return HttpNotFound();
         }
 
-        public ActionResult Edit(int id)
-        {
-            return Content($"id = {id}");
-        }
+        //public ActionResult Edit(int id)
+        //{
+        //    return Content($"id = {id}");
+        //}
 
         public ActionResult Index(int? pageIndex, string sortBy)
         {
