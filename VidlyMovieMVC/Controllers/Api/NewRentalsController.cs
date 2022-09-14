72ab03c0110f9c7ce6162d1990260d2c4dcb8603
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-09-14 12:55:37 +0200

    added model and api controller for rentals

---
+++ VidlyMovieMVC/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VidlyMovieMVC.Dtos;
+
+namespace VidlyMovieMVC.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRentalDto)
+        {
+
+            return Ok();
+        }
+    }
+}
