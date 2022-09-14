125ad4ae6ffb2abcca42b7759213b6ee48e38917
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-09-14 14:20:34 +0200

    Adding Auto-completion for customers

--- VidlyMovieMVC/Controllers/Api/CustomersController.cs
+++ VidlyMovieMVC/Controllers/Api/CustomersController.cs
@@ -22,14 +22,19 @@ namespace VidlyMovieMVC.Controllers.Api
 
         // GET /api/customers
         [HttpGet]
-        public IHttpActionResult GetCustomers()
+        public IHttpActionResult GetCustomers(string query = null)
         {
-            var customers = _context.Customers
-                .Include(c => c.MembershipType)
+            var customersQuery = _context.Customers
+                .Include(c => c.MembershipType);
+
+            if (!String.IsNullOrWhiteSpace(query))
+                customersQuery = customersQuery.Where(c => c.Name.Contains(query));
+
+            var customerDtos = customersQuery
                 .ToList()
                 .Select(Mapper.Map<Customer, CustomerDto>);
 
-            return Ok(customers);
+            return Ok(customerDtos);
         }
 
         // GET /api/customers/id
