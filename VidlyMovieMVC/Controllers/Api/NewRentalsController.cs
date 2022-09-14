fa6e65cf088dd6fe0bac870a90f42bdb530e3866
Oskar Lewandowski <osklew1@st.amu.edu.pl>
2022-09-14 13:12:19 +0200

    added NumberAvailable in model movie

--- VidlyMovieMVC/Controllers/Api/NewRentalsController.cs
+++ VidlyMovieMVC/Controllers/Api/NewRentalsController.cs
@@ -5,16 +5,49 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using VidlyMovieMVC.Dtos;
+using VidlyMovieMVC.Models;
 
 namespace VidlyMovieMVC.Controllers.Api
 {
     public class NewRentalsController : ApiController
     {
-        [HttpPost]
-        public IHttpActionResult CreateNewRentals(NewRentalDto newRentalDto)
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
         {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            var customer = _context.Customers.Single(
+                c => c.Id == newRental.CustomerId);
+
+            var movies = _context.Movies.Where(
+                m => newRental.MovieIds.Contains(m.Id)).ToList();
+
+            foreach (var movie in movies)
+            {
+                if (movie.NumberAvailable == 0)
+                    return BadRequest("Movie is not available.");
+
+                movie.NumberAvailable--;
+
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
 
             return Ok();
         }
+
     }
 }
